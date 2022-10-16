f46521e85a9faba0c837bab48703c7cc37c6fe80
mariyaGorobec <mariyagorobec.2001@gmail.com>
2022-10-16 21:11:21 +0800

    Добавление хука useState для кнопки добавления товара

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,6 @@
 import styles from './Card.module.scss'
+import React from 'react';
+
 
 function divideNumberIntoСategory(num) {
   let n = num.toString();
@@ -6,10 +8,14 @@ function divideNumberIntoСategory(num) {
 }
 
 function Card(props) {
+  const [isAdded, setIsAdded] = React.useState(false);
+  const onClickAdd = () =>{
+    setIsAdded(!isAdded);
+  } 
   return (
     <div className={styles.card}>
       <div className={styles.cardTop}>
-        <div className={styles.favorite}>
+        <div className={styles.favorite} onClick={props.onFavorite}>
           <svg
             width="32"
             height="32"
@@ -47,20 +53,15 @@ function Card(props) {
           <span> Цена:</span>
           <b>{divideNumberIntoСategory(props.price)} руб.</b>
         </div>
-        <button onClick={()=>alert("Нажали на плюс")}>
-          <svg
-            width="15"
-            height="15"
-            viewBox="0 0 12 12"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M10.6653 5.13128H7.20219V1.66827C7.20219 0.332907 5.13118 0.332907 5.13118 1.66827V5.13128H1.66805C0.332981 5.13128 0.332981 7.20221 1.66805 7.20221H5.13118V10.6652C5.13118 12.0006 7.20219 12.0006 7.20219 10.6652V7.20221H10.6653C12.0006 7.20221 12.0006 5.13128 10.6653 5.13128Z"
-              fill="#f47c77"
-            />
-          </svg>
-        </button>
+        <img
+        alt = "Кнопка добавить или кнопка добавленно"
+        className={styles.plus}
+        onClick={onClickAdd}
+         width="15"
+         height="15"
+         src={isAdded ? "/img/btn-checked.svg":"/img/plus.svg"}>
+         </img>
+          
       </div>
     </div>
   );
